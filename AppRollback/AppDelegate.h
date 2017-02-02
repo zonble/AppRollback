4408d2fd480b80b5cf4bdeabced315d08ade8a33
zonble <zonble@gmail.com>
2017-02-02 23:18:29 +0800

    Initial Commit

---
+++ AppRollback/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  AppRollback
+//
+//  Created by zonble on 2/2/17.
+//  Copyright © 2017 KKBOX Taiwan Co., Ltd. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
